4946efd3e7b15c46396de2fb43c435f20dc853ce
ArenaZilkha2023 <noga.karp@weizmann.ac.il>
2025-05-08 18:36:10 +0300

    "Final Script"

--- plot_data.py
+++ plot_data.py
@@ -46,8 +46,10 @@ class plot_data:
       match type_graph:
              case "shap_features_violin":
                    self.PlotShape(model_name) 
-             case "random_permutation":
-                    self.PlotRandomPermutation(model_name)      
+             case "random_permutation_plus_CM" :
+                    self.Plot_random_cm(model_name) 
+             case "shap_features_dotpoints":
+                    self.shap_features_dotpoints(model_name)
           
     '''
     input: shap data plus features
@@ -86,37 +88,9 @@ class plot_data:
           self.X_top_females = self.X_top.iloc[self.female_row_num,:]
           
     
-    '''
-    plot random permutation
-    '''
-    def PlotRandomPermutation(self,model_name):
-           class_ = self.data[model_name]['classes']
-           fig , axs = plt.subplots(1, 2, figsize=(10, 5))
-           fig.suptitle(tuple(self.data[model_name]['classes']), fontsize=10)
-           for index in range(2):
-              plt.sca(axs[index]) 
-              axs[index].set_axis_on()
-              fscore_actual = (self.data[model_name]['fscore'])[index]
-              class_num =str(index +1)
-              pval = np.mean(self.data[model_name]['shuffle_fscore_class'+ class_num] >= fscore_actual)
-              plt.hist(self.data[model_name]['shuffle_fscore_class'+ class_num ], bins=6, alpha=0.7, color='blue', edgecolor='black')#bin 6
-              plt.axvline(fscore_actual, color='red',linestyle='--', label= f"Actual F-score = {fscore_actual}")
-              plt.text(fscore_actual + 0.1, plt.ylim()[1]*0.8, f"pvalue = {pval:.3f}", color='red',fontsize = 6)
-              axs[index].set_title('random permutation',fontsize=8)
-              axs[index].set_xlim(0,1)
-              axs[index].set_ylabel("Frequency")
-              axs[index].set_xlabel('shuffle_fscore_' + class_[index])
-              axs[index].xaxis.label.set_size(6)
-              axs[index].yaxis.label.set_size(6)
-           for tick in axs[index].get_yticklabels():
-             tick.set_fontsize(6)
-           for tick in axs[index].get_xticklabels():
-             tick.set_fontsize(6)  
-             
-           plt.tight_layout()
-            #plt.show()
-           plt.savefig(self.output_directory + self.title  +'_random_permutation.pdf', format='pdf',dpi=300,bbox_inches='tight') 
-      
+    
+       
+   
     
     '''
     plot shape values
@@ -142,7 +116,49 @@ class plot_data:
       plt.tight_layout()
       #plt.show()
       plt.savefig(self.output_directory + self.title  +'.pdf', format='pdf',dpi=300,bbox_inches='tight') 
-      
+    
+    '''
+    Plot metrics data
+    '''
+    def Plot_random_cm(self, model_name): 
+           
+           fig , axs = plt.subplots(2, 2, figsize=(10, 10))
+           fig.suptitle(tuple(self.data[model_name]['classes']), fontsize=10)  
+           for i in range(4):
+            axs[i // 2, i % 2].set_axis_off()
+           #get random permutation
+            ax = axs[0,0]
+            self.PlotRandomPermutation(ax, model_name,0) #class 0
+            ax = axs[0,1]
+            self.PlotRandomPermutation(ax, model_name,1) #class 1
+            ax = axs[1,0]
+            self.PlotConfusionMatrix(ax,model_name)
+            
+            plt.tight_layout()
+            #plt.show()
+            plt.savefig(self.output_directory + self.title  +'_random_permutation_CM.pdf', format='pdf',dpi=300,bbox_inches='tight') 
+     
+    '''
+     Plot features and dot points
+     '''       
+    def shap_features_dotpoints(self, model_name):
+           fig , axs = plt.subplots(1, 2, figsize=(4, 4))
+           fig.suptitle(tuple(self.data[model_name]['classes']), fontsize=10)  
+           axs[0].set_axis_off()  
+           axs[1].set_axis_off()
+           #get important features
+           ax = axs[0]
+           self.plotImportantFeatures(ax, model_name)
+           #get dot plots
+           ax = axs[1]
+           self.plotDotFeatures(ax,model_name)
+            
+           plt.tight_layout()
+            #plt.show()
+           plt.savefig(self.output_directory + self.title  +'_Features_dotPoints.pdf', format='pdf',dpi=300,bbox_inches='tight') 
+           
+            
+            
     '''
     Important features
     '''
@@ -387,3 +403,48 @@ class plot_data:
        for tick in ax.get_xticklabels():
               tick.set_fontsize(6)
     
+    
+    '''
+    plot random permutation
+    '''
+    def PlotRandomPermutation(self,ax, model_name,index):
+           
+           class_ = self.data[model_name]['classes']
+           plt.sca(ax) 
+           ax.set_axis_on()
+           fscore_actual = (self.data[model_name]['fscore'])[index]
+           class_num =str(index +1)
+           pval = np.mean(self.data[model_name]['shuffle_fscore_class'+ class_num] >= fscore_actual)
+           plt.hist(self.data[model_name]['shuffle_fscore_class'+ class_num ], bins=6, alpha=0.7, color='blue', edgecolor='black')#bin 6
+           plt.axvline(fscore_actual, color='red',linestyle='--', label= f"Actual F-score = {fscore_actual}")
+           plt.text(fscore_actual + 0.1, plt.ylim()[1]*0.8, f"pvalue = {pval:.3f}", color='red',fontsize = 6)
+           ax.set_title('random permutation',fontsize=8)
+           ax.set_xlim(0,1)
+           ax.set_ylabel("Frequency")
+           ax.set_xlabel('shuffle_fscore_' + class_[index])
+           ax.xaxis.label.set_size(6)
+           ax.yaxis.label.set_size(6)
+           for tick in ax.get_yticklabels():
+             tick.set_fontsize(6)
+           for tick in ax.get_xticklabels():
+             tick.set_fontsize(6)  
+             
+    '''
+   Plot confusion matrix
+   '''        
+    def PlotConfusionMatrix(self,ax,model_name):
+        try:
+                  cm = self.data[model_name]['confusion_matrix']
+                  #get classes
+                  class_names = self.data[model_name]['classes']
+                  #divide each number by its row sum
+                  cm_percent = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]  #new axis row row
+             
+                  ax.set_axis_on()
+                  #Normalize by the total number of instances per class
+                  sns.heatmap(cm_percent, annot=True, fmt='.2%', cmap='Blues', cbar=False,xticklabels=class_names, yticklabels=class_names,annot_kws={"size": 8},ax =ax)
+                  
+                  ax.set_xlabel('Predicted Labels')
+                  ax.set_ylabel('True Labels')
+        except Exception as e:
+                  print("error")   
